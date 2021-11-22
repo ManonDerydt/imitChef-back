526a0bb2cf7bdd2ef05dd1f45675b2ab27737a54
Manon <mderydt@libheros.fr>
2021-11-22 17:53:22 +0100

    En cours de dev

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const models = require("./models");
 
 const app = express();
+app.get("/", (req, res, next) => res.send("OK"));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Header', 'Origin');
-  res.setHeader('Access-Control-Allow-Methods', 'GET','POST', 'PUT', 'DELETE', 'PATCH');
-  next();
-})
 
-app.use(bodyparser.json());
+(async() => {
+    try {
+        await models.sequelize.authenticate();
 
-app.post('/aoi/stuff', (req, res, next) => {
-  console.log(req.body);
-})
-
-app.use('/api/reciepes', (req, res, next) => {
-  const reciepes = [
-    {
-      _id: '1',
-      title: 'Choux de Bruxelles',
-      description: 'Hello world !'
-    },
-    {
-      _id: '2',
-      title: 'Noix de saint jacques',
-      description: 'Hello earth !'
+        const users = await models.User.findAll()
+        console.log(users)
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
     }
-  ]
-})
+})();
 
 module.exports = app;
