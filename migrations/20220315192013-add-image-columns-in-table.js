5ee159eda4d1592a3ba131b7b092a3d9557eb347
marc <bernini.marc@gmail.com>
2022-03-15 20:58:37 +0100

    Suppression du dossier de configuration pour le mettre dans le .gitignore
    Suppression du fichier ionic.config.json inutile ici
    
    Suppression de la migration 'users' inutile
    
    Ajout d'une migration pour ajouter les colonnes 'images' pour les tables 'Candidates', 'Tv_show' et 'Reciepes'. Ajout des colonnes 'titre' et 'description' dans la table 'Reciepes'
    
    Nettoyage des routes + ajout de la route pour récupérer un candidat avec ses recettes. Ajout des attributs dans la récupération d'une émission avec ses participants et leurs recettes

---
+++ migrations/20220315192013-add-image-columns-in-table.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    try {
+      await queryInterface.addColumn("Candidates", "image", {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+      await queryInterface.addColumn("Reciepes", "image", {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+      await queryInterface.addColumn("Reciepes", "description", {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+      await queryInterface.addColumn("Reciepes", "titre", {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+      await queryInterface.addColumn("Tv_show", "image", {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    try {
+      await queryInterface.removeColumn("Candidates", "image");
+      await queryInterface.removeColumn("Reciepes", "image");
+      await queryInterface.removeColumn("Reciepes", "titre");
+      await queryInterface.removeColumn("Reciepes", "description");
+      await queryInterface.removeColumn("Tv_show", "image");
+    } catch (error) {
+      throw error;
+    }
+  }
+};
--- modules/reciepes_from_candidates/reciepefromCandidates.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const reciepeFromCandidates = require("./reciepefromCandidates.middlewares");
-
-// router.route("/reciepe-from-candidates").get(reciepeFromCandidates.getRecipeFromCandidates);
-
-module.exports = router;
