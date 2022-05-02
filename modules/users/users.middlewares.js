59f4cb6386a1b7d0cbccc4de4116cb2b1e2857e6
manon <derydtmanon@gmail.com>
2022-05-02 21:15:29 +0200

    Ajout de la route et de la fonction pour authentifier le User

--- modules/users/users.middlewares.js
+++ modules/users/users.middlewares.js
@@ -2,6 +2,8 @@ const path = require("path");
 const models = require(path.resolve("./models"));
 const debug = require("debug")("app:users");
 const bcryptService = require(path.resolve("./services/bcrypt"))
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
 
 exports.createUsers = (req, res, next) => {
     debug("createUser");
@@ -13,8 +15,8 @@ exports.createUsers = (req, res, next) => {
             res.json({"Ok": true});
         })
         .catch((err) => {
-            if( err.message === "UNIQUE_EMAIL"){
-                return res.status(401).json({message : "L'adresse email est déjà utilisée"})
+            if (err.message === "UNIQUE_EMAIL") {
+                return res.status(401).json({message: "L'adresse email est déjà utilisée"})
             }
             next(err)
         });
@@ -23,18 +25,65 @@ exports.createUsers = (req, res, next) => {
         try {
             const hashPassword = await bcryptService.hashPassword(password)
             const user = {
-                password : hashPassword,
-                email : email,
-                lastname : lastname,
-                firstname : firstname
+                password: hashPassword,
+                email: email,
+                lastname: lastname,
+                firstname: firstname
             }
             return await models.User.create(user);
-        }
-        catch(err) {
-            if(err.name === "SequelizeUniqueConstraintError"){
+        } catch (err) {
+            if (err.name === "SequelizeUniqueConstraintError") {
                 throw Error("UNIQUE_EMAIL")
             }
             throw (err);
+        }
+    }
+}
+
+exports.loginUsers = (req, res, next) => {
+    debug("loginUsers");
+
+    return execute()
+        .then(user => res.json(user))
+        .catch((err) => {
+            if (err === "NO_USER") {
+                res.status(403).json({message: "Authentification échouée"});
+            } else {
+                next(err);
+            }
+        });
+
+    async function execute() {
+        try {
+            const user = await new Promise(logUserWithPassport);
+            const token = await generateJWT(user)
+            return {user, token};
+
+        } catch (err) {
+            throw (err);
+        }
+
+        function logUserWithPassport(resolve, reject) {
+            passport.authenticate("local", {session: false}, (err, user, info) => {
+                if (err || !user) {
+                    return reject(info.message);
+                }
+                return resolve(user);
+            })(req, res, next)
+        }
+
+        function generateJWT(user) {
+            return new Promise((resolve, reject) => {
+                req.login(user, {session: false}, async (err) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    const {id, email} = user;
+                    const token = await jwt.sign({id, email}, process.env.JWT_SECRET);
+                    return resolve(token)
+                });
+            })
+
         }
     }
 }
--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const users = require("./users.middlewares");
 
 router.route("/users").post(users.createUsers);
+router.route("/users/login").post(users.loginUsers);
 
 module.exports = router;
