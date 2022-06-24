645ce888e616e3adeda6d1f578f1c37a300c0e55
manon <derydtmanon@gmail.com>
2022-06-24 14:33:44 +0200

    Suppression d'un console.log

--- modules/restaurants/restaurants.routes.js
+++ modules/restaurants/restaurants.routes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const restaurants = require("./restaurants.middlewares");
 
-console.log("Hello, je suis à la route restaurant !")
 router.route("/restaurant/:id").get(restaurants.getRestaurant);
 
 module.exports = router;
