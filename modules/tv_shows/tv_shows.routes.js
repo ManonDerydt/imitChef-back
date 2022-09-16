ff9caa7dcf95ff4cd942d6c27d98c8ce51c9605d
manon <derydtmanon@gmail.com>
2022-09-16 23:25:58 +0200

    Récupération de la route vers Galery

--- modules/tv_shows/tv_shows.routes.js
+++ modules/tv_shows/tv_shows.routes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const tvShows = require("./tv_shows.middlewares");
 const candidates = require("../candidates/candidates.middlewares");
+const gallery = require("../gallery/gallery.middlewares");
 
 router.route("/show").get(tvShows.getAllShow);
 router.route("/show/:id").get(tvShows.getShow);
+router.route("/gallery").get(gallery.getGallery)
 // router.route("/candidatesByShow").get(tvShows.getCandidatesByShow);
 
 module.exports = router;
